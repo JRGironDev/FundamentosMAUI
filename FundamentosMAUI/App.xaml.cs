b983aa7d354b4b98d082ea6d1beb1843bee3aa49
José Quiñónez <joserobertoq@outlook.com>
2024-01-15 12:28:58 -0600

    Ciclos de vida

--- FundamentosMAUI/App.xaml.cs
+++ FundamentosMAUI/App.xaml.cs
@@ -6,10 +6,63 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new ContentPage()
-		{
-			Content = new Label() { Text = new NameDevice().GetName() }
+        var boton = new Button();
+
+        boton.Text = "Generar nueva ventana";
+        boton.Clicked += (s, a) =>
+        {
+            var ventana2 = new Window(new ContentPage());
+            ventana2.Created += (s, a) => System.Diagnostics.Debug.WriteLine(nameof(ventana2.Created));
+            ventana2.Deactivated += (s, a) => System.Diagnostics.Debug.WriteLine(nameof(ventana2.Deactivated));
+            ventana2.Activated += (s, a) => System.Diagnostics.Debug.WriteLine(nameof(ventana2.Activated));
+            OpenWindow(ventana2);
+        };
+
+        MainPage = new ContentPage()
+        {
+            Content = boton
 		};
 	}
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var ventana = base.CreateWindow(activationState);
+        ventana.Created += Ventana_Created;
+        ventana.Deactivated += Ventana_Deactivated;
+        ventana.Activated += Ventana_Activated;
+        return ventana;
+    }
+
+    private void Ventana_Activated(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(nameof(Ventana_Activated));
+    }
+
+    private void Ventana_Deactivated(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(nameof(Ventana_Deactivated));
+    }
+
+    private void Ventana_Created(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(nameof(Ventana_Created));
+    }
 }
+
+
 
